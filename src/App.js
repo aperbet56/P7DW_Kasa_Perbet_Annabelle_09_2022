0948499acd757a7d954be16f20ee478b4d1f3d79
Annabelle Perbet <annabelleperbet@yahoo.fr>
2022-10-05 15:35:45 +0200

    Création des différentes routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Accomodation from "./pages/Accomodation";
+import Error from "./pages/Error";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/accomodation/:id" element={<Accomodation />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
