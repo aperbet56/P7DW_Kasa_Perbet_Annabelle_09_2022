98d94aa6c9d1280c73f22e2330954dd7d3bf3f10
Annabelle Perbet <annabelleperbet@yahoo.fr>
2022-10-05 16:05:53 +0200

    Mise à jour de la page home (page d'accueil)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import Card from "../components/Card";
+import Footer from "../components/Footer";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Card />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
