1efbbeb342b0be449c8a4aa8c4ba1b22a2d7a5c5
Annabelle Perbet <annabelleperbet@yahoo.fr>
2022-10-05 16:42:25 +0200

    Création de la page erreur 404

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
 
 const Error = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <section className="error404">
+        <h1 className="error404__title">404</h1>
+        <p className="error404__text">
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+        <Link to="/" className="back-home__link">
+          Retourner sur la page d'accueil.
+        </Link>
+      </section>
+    </div>
+  );
 };
 
 export default Error;
