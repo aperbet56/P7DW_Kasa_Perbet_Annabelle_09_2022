fb79ef6a118e85afbab86f795965e81174e60926
Annabelle Perbet <annabelleperbet@yahoo.fr>
2022-10-05 17:32:07 +0200

    Mise en place du style pour la bannière de la page A propos

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,25 @@
 import React from "react";
+import Header from "../components/Header";
+import AboutBanner from "../components/AboutBanner";
+import Collapse from "../components/Collapse";
+import Data from "../components/AccordionData";
+import Footer from "../components/Footer";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <main>
+        <AboutBanner />
+        <section className="about__collapse">
+          {Data.map((item, index) => (
+            <Collapse title={item.title} content={item.content} key={index} />
+          ))}
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default About;
