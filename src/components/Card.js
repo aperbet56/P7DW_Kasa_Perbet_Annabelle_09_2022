ce745d5d251dd1420c92bc12ca14dddaffaab7fd
Annabelle Perbet <annabelleperbet@yahoo.fr>
2022-10-11 14:51:04 +0200

    Déplacement du fetch dans la page home + mise à jour du composant card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,46 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
 
-const Card = () => {
-  const [data, setData] = useState([]);
-
-  const getData = () => {
-    fetch("logements.json", {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    })
-      .then(function (response) {
-        console.log(response);
-        return response.json();
-      })
-
-      .then(function (myJson) {
-        console.log(myJson);
-        setData(myJson);
-      });
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
+const Card = ({ accomodation }) => {
   return (
-    <section className="accomodations">
-      {data.map((accomodation) => (
-        <Link
-          to={`accomodation/${accomodation.id}`}
-          key={accomodation.id}
-          className="card"
-        >
-          <figure className="card__item">
-            <img src={`${accomodation.cover}`} alt={accomodation.title} />
-            <figcaption>{accomodation.title}</figcaption>
-          </figure>
-        </Link>
-      ))}
-    </section>
+    <figure className="card__item">
+      <img src={`${accomodation.cover}`} alt={accomodation.title} />
+      <figcaption>{accomodation.title}</figcaption>
+    </figure>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,57 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 import Card from "../components/Card";
 import Footer from "../components/Footer";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+
+  const getData = () => {
+    fetch("logements.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+
+      .then(function (data) {
+        console.log(data);
+        setData(data);
+      })
+      .catch(function (error) {
+        console.log(error);
+        setError(error);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (error) {
+    return <p>Oups, il y a eu un problème.</p>;
+  }
+
   return (
     <div>
       <Header />
       <main>
         <Banner />
-        <Card />
+        <section className="accomodations">
+          {data.map((accomodation, index) => (
+            <Link key={index} to={`accomodation/${accomodation.id}`}>
+              <Card key={index} accomodation={accomodation} />
+            </Link>
+          ))}
+        </section>
       </main>
       <Footer />
     </div>
