02c57a4314183b1bb0cbd0bafffe741b761a2fbd
Annabelle Perbet <annabelleperbet@yahoo.fr>
2022-10-22 19:39:38 +0200

    Correction mineure du code

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ accomodation }) => {
   return (
     <figure className="card__item">
-      <img src={`${accomodation.cover}`} alt={accomodation.title} />
+      <img src={accomodation.cover} alt={accomodation.title} />
       <figcaption>{accomodation.title}</figcaption>
     </figure>
   );
