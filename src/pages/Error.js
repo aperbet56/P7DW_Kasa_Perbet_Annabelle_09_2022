493a98d4e3bf09ddf819ffafb8996b0548acc134
Annabelle Perbet <annabelleperbet@yahoo.fr>
2022-10-23 21:00:17 +0200

    Ajout de la balise <main>

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -7,15 +7,17 @@ const Error = () => {
   return (
     <div>
       <Header />
-      <section className="error404">
-        <h1 className="error404__title">404</h1>
-        <p className="error404__text">
-          Oups! La page que vous demandez n'existe pas.
-        </p>
-        <Link to="/" className="back-home__link">
-          Retourner sur la page d'accueil.
-        </Link>
-      </section>
+      <main>
+        <section className="error404">
+          <h1 className="error404__title">404</h1>
+          <p className="error404__text">
+            Oups! La page que vous demandez n'existe pas.
+          </p>
+          <Link to="/" className="back-home__link">
+            Retourner sur la page d'accueil.
+          </Link>
+        </section>
+      </main>
       <Footer />
     </div>
   );
